fab2c657604a37459806ce894845932f5dc6f721
oeodeoie <143570754+32e23443232@users.noreply.github.com>
2024-03-09 21:23:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to usa","Watching USA"];
 
 
 let currentIndex = 0;
